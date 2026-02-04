e869c0dde50e6c5385bae82c75c403b64d81aa60
Daniel Pérez <daniel.perez@adsmurai.com>
2026-02-04 22:23:05 +0100

    fix: remove deprecated KanagawaCompile build step

--- private_dot_config/nvim/lua/plugins/kanagawa.lua
+++ private_dot_config/nvim/lua/plugins/kanagawa.lua
@@ -1,24 +1,23 @@
 return {
   "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
     require("kanagawa").setup({
       compile = true,
       transparent = true,
-      overrides = function ()
+      overrides = function()
         return {
-          ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
-          ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
-          ["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
-          ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
-          ["@markup.list.markdown"] = { link = "Function" }, -- + list
-          ["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
-          ["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- - [X] checked list item
+          ["@markup.link.url.markdown_inline"] = { link = "Special" },
+          ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" },
+          ["@markup.italic.markdown_inline"] = { link = "Exception" },
+          ["@markup.raw.markdown_inline"] = { link = "String" },
+          ["@markup.list.markdown"] = { link = "Function" },
+          ["@markup.quote.markdown"] = { link = "Error" },
+          ["@markup.list.checked.markdown"] = { link = "WarningMsg" },
         }
-      end
-    });
-    vim.cmd("colorscheme kanagawa");
-  end,
-  build = function()
-    vim.cmd("KanagawaCompile");
+      end,
+    })
+    vim.cmd.colorscheme("kanagawa")
   end,
 }
