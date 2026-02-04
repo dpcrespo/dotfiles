e9a628d6371e7686c95a68ba6bb55382a0c0eceb
Daniel Pérez <daniel.perez@adsmurai.com>
2026-02-04 22:20:54 +0100

    fix: update treesitter config to use explicit config function

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,46 +4,64 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = ":TSUpdate",
-  event = "VeryLazy",
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "tsx", "scss", "css", "html", "json", "jsonc", "markdown", "markdown_inline", "yaml" },
-    auto_install = false,
-    sync_install = false,
-    highlight = {
-      enable = true,
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-      end,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@conditional.outer",
-          ["ic"] = "@conditional.inner",
-          ["al"] = "@loop.outer",
-          ["il"] = "@loop.inner",
-        },
-        include_surrounding_whitespace = true,
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "typescript",
+        "tsx",
+        "scss",
+        "css",
+        "html",
+        "json",
+        "jsonc",
+        "markdown",
+        "markdown_inline",
+        "yaml",
       },
-      swap = {
+      auto_install = false,
+      sync_install = false,
+      highlight = {
         enable = true,
-	      swap_next = {
-	        ["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next parameter" },
-	      },
-        swap_previous = {
-	        ["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous parameter" },
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+      indent = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+          },
+          include_surrounding_whitespace = true,
         },
-	    },
-    },
-    additional_vim_regex_highlighting = false,
-  },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = { query = "@parameter.inner", desc = "Swap with next parameter" },
+          },
+          swap_previous = {
+            ["<leader>A"] = { query = "@parameter.inner", desc = "Swap with previous parameter" },
+          },
+        },
+      },
+    })
+  end,
 }
